8d5625c2e426a7f5834c11f5e9455afd71ac8d3b
Ap1raya <saynakhax@gmail.com>
2025-09-03 15:17:06 +0700

    k

---
+++ agent-wallboard-learn/server.js
@@ -0,0 +1,56 @@
+
+const express = require('express');
+const app = express();
+const PORT = 3001;
+/*
+AVAILABLE  = พร้อมรับสาย
+ACTIVE     = กำลังคุยกับลูกค้า  
+WRAP_UP    = บันทึกหลังจบสาย
+NOT_READY  = ไม่พร้อมรับสาย (พัก/ประชุม)
+OFFLINE    = ออฟไลน์
+*/
+
+
+const agent = [{
+    code: "A001",
+    name: Noah,
+    status: _____,
+},
+{
+    const agent =
+        code: "A001",        // รหัส Agent
+    name: Liam,         // เติมคิดเอง
+    status: _____,       // เติมคิดเอง  
+    // คิดต่อว่าควรมีอะไรอีก...
+},
+{
+
+const agent = 
+    code: "A001",        // รหัส Agent
+    name: Leo,         // เติมคิดเอง
+    status: _____,       // เติมคิดเอง  
+    // คิดต่อว่าควรมีอะไรอีก...
+}
+];
+
+app.get('/hello', (req, res) => {
+
+
+    res.sendStatus("สวัสดี ! ");
+}); // // เติม method และ response function
+
+app.get('/health', (req, res) => {
+
+
+    res.json(
+        {
+            "status": "OK",
+            "timestamp": "เวลาปัจจุบัน"
+        });
+
+}); // // เติม method และ response function
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
